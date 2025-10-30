b5f5258229aef83ab23c36ded98ed4098a6243b2
MATHEUS KANSBOCK <23.01182-3@maua.br>
2025-10-30 19:04:10 -0300

    Correção na api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AlunoModule } from './aluno/aluno.module';
 import { Aluno } from './aluno/aluno.entity';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '3306', 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '3306', 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Aluno],
       synchronize: process.env.NODE_ENV !== 'production',
     }),
